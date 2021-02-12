d3a6b33184f37104a32688ac82a9179cb0a581b8
Crowdbotics <team@crowdbotics.com>
2021-02-12 17:36:32 +0000

    Initial commit.

---
+++ backend/nikolinas_testing_g_24471/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nikolinas_testing_g_24471 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nikolinas_testing_g_24471.settings')
+
+application = get_wsgi_application()
